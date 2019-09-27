d2ba7ecb403afead7c46e3f3148204f6fdbd35a9
Fabio Rachid <fabiorbap@gmail.com>
2019-09-26 21:31:49 -0300

    implemented tests and implementation

--- test_driven_development/src/main/java/com/techyourchance/testdrivendevelopment/exercise6/FetchUserUseCaseSyncImpl.java
+++ test_driven_development/src/main/java/com/techyourchance/testdrivendevelopment/exercise6/FetchUserUseCaseSyncImpl.java
@@ -1,8 +1,36 @@
 package com.techyourchance.testdrivendevelopment.exercise6;
 
+import com.techyourchance.testdrivendevelopment.exercise6.networking.FetchUserHttpEndpointSync;
+import com.techyourchance.testdrivendevelopment.exercise6.networking.NetworkErrorException;
+import com.techyourchance.testdrivendevelopment.exercise6.users.User;
+import com.techyourchance.testdrivendevelopment.exercise6.users.UsersCache;
+
 public class FetchUserUseCaseSyncImpl implements FetchUserUseCaseSync {
+
+    private FetchUserHttpEndpointSync fetchUserHttpEndpointSync;
+    private UsersCache usersCache;
+
+    public FetchUserUseCaseSyncImpl(FetchUserHttpEndpointSync fetchUserHttpEndpointSync, UsersCache usersCache) {
+        this.fetchUserHttpEndpointSync = fetchUserHttpEndpointSync;
+        this.usersCache = usersCache;
+    }
+
     @Override
     public UseCaseResult fetchUserSync(String userId) {
-        return null;
+        FetchUserHttpEndpointSync.EndpointResult result;
+        try {
+            result = fetchUserHttpEndpointSync.fetchUserSync(userId);
+        } catch (NetworkErrorException e) {
+            return new UseCaseResult(Status.NETWORK_ERROR, null);
+        }
+
+        if (result.getStatus() == FetchUserHttpEndpointSync.EndpointStatus.AUTH_ERROR || result.getStatus() == FetchUserHttpEndpointSync.EndpointStatus.GENERAL_ERROR){
+            return new UseCaseResult(Status.FAILURE, null);
+        } else {
+            if (usersCache.getUser(userId) != null) {
+                usersCache.cacheUser(new User(result.getUserId(), result.getUsername()));
+            }
+        }
+        return new UseCaseResult(Status.SUCCESS, usersCache.getUser(userId));
     }
 }
--- test_driven_development/src/test/java/com/techyourchance/testdrivendevelopment/exercise6/FetchUserUseCaseSyncImplTest.java
+++ test_driven_development/src/test/java/com/techyourchance/testdrivendevelopment/exercise6/FetchUserUseCaseSyncImplTest.java
@@ -2,11 +2,17 @@ package com.techyourchance.testdrivendevelopment.exercise6;
 
 import com.techyourchance.testdrivendevelopment.exercise6.networking.FetchUserHttpEndpointSync;
 import com.techyourchance.testdrivendevelopment.exercise6.networking.NetworkErrorException;
+import com.techyourchance.testdrivendevelopment.exercise6.users.User;
 import com.techyourchance.testdrivendevelopment.exercise6.users.UsersCache;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Objects;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -14,8 +20,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class FetchUserUseCaseSyncImplTest {
 
     private static String USER_ID = "user_id";
@@ -30,18 +38,15 @@ public class FetchUserUseCaseSyncImplTest {
     private FetchUserHttpEndpointSync fetchUserHttpEndpointSync;
 
     @Before
-    void setup() {
-        SUT = new FetchUserUseCaseSyncImpl();
+    public void setup() throws NetworkErrorException {
+        SUT = new FetchUserUseCaseSyncImpl(fetchUserHttpEndpointSync, usersCache);
+        success();
     }
-
-    /*1) If the user with given user ID is not in the cache then it should be fetched from the server.
-            2) If the user fetched from the server then it should be stored in the cache before returning to the caller.
-            3) If the user is in the cache then cached record should be returned without polling the server.*/
 
     // Passed parameters to function
 
-    @Before
-    void fetchUser_passedParameters_success() {
+    @Test
+    public void fetchUser_passedParameters_success() {
         ArgumentCaptor<String> ac = ArgumentCaptor.forClass(String.class);
         SUT.fetchUserSync(USER_ID);
         verify(SUT, times(1)).fetchUserSync(ac.capture());
@@ -50,34 +55,86 @@ public class FetchUserUseCaseSyncImplTest {
     }
 
     // fetch user success, user stored in cache
-    @Before
-    void fetchUser_success_userCached() {
-
+    @Test
+    public void fetchUser_success_userCached() {
+        ArgumentCaptor<User> ac = ArgumentCaptor.forClass(User.class);
+        SUT.fetchUserSync(USER_ID);
+        verify(usersCache).cacheUser(ac.capture());
+        User capture = ac.getValue();
+        assertThat(capture.getUserId(), is(USER_ID));
+        assertThat(capture.getUsername(), is(USER_NAME));
     }
 
     // fetch user success, user already in cache
+
+    @Test
+    public void fetchUser_success_userCorrectlyCached() {
+        cacheUser();
+        ArgumentCaptor<User> ac = ArgumentCaptor.forClass(User.class);
+        SUT.fetchUserSync(USER_ID);
+        verify(usersCache, times(0)).cacheUser(ac.capture());
+    }
+
     // fetch user success, success returned
-    // fetch user server error, server failure returned
+
+    @Test
+    public void fetchUser_success_successReturned() throws NetworkErrorException {
+        FetchUserUseCaseSync.UseCaseResult result = SUT.fetchUserSync(USER_ID);
+        verify(fetchUserHttpEndpointSync).fetchUserSync(USER_ID);
+        assertThat(result.getStatus(), is(FetchUserUseCaseSync.Status.SUCCESS));
+    }
+
+    // fetch user auth error, server failure returned
+
+    @Test
+    public void fetchUser_authError_errorReturned() throws NetworkErrorException {
+        authError();
+        FetchUserUseCaseSync.UseCaseResult result = SUT.fetchUserSync(USER_ID);
+        verify(fetchUserHttpEndpointSync).fetchUserSync(USER_ID);
+        assertThat(result.getStatus(), is(FetchUserUseCaseSync.Status.FAILURE));
+    }
+
     // fetch user network error, network error returned
+
+    @Test
+    public void fetchUser_networkError_errorReturned() throws NetworkErrorException {
+        networkError();
+        FetchUserUseCaseSync.UseCaseResult result = SUT.fetchUserSync(USER_ID);
+        verify(fetchUserHttpEndpointSync).fetchUserSync(USER_ID);
+        assertThat(result.getStatus(), is(FetchUserUseCaseSync.Status.NETWORK_ERROR));
+    }
+
     // fetch user general error, general error returned
 
+    @Test
+    public void fetchUser_generalError_errorReturned() throws NetworkErrorException {
+        generalError();
+        FetchUserUseCaseSync.UseCaseResult result = SUT.fetchUserSync(USER_ID);
+        verify(fetchUserHttpEndpointSync).fetchUserSync(USER_ID);
+        assertThat(result.getStatus(), is(FetchUserUseCaseSync.Status.FAILURE));
+    }
+
     private void success() throws NetworkErrorException {
-        when(fetchUserHttpEndpointSync.fetchUserSync(USER_ID)).thenReturn(new FetchUserHttpEndpointSync.EndpointResult(FetchUserHttpEndpointSync.EndpointStatus.SUCCESS,
+        when(fetchUserHttpEndpointSync.fetchUserSync(any(String.class))).thenReturn(new FetchUserHttpEndpointSync.EndpointResult(FetchUserHttpEndpointSync.EndpointStatus.SUCCESS,
                 USER_ID, USER_NAME));
     }
 
     private void generalError() throws NetworkErrorException {
-        when(fetchUserHttpEndpointSync.fetchUserSync(USER_ID)).thenReturn(new FetchUserHttpEndpointSync.EndpointResult(FetchUserHttpEndpointSync.EndpointStatus.GENERAL_ERROR,
+        when(fetchUserHttpEndpointSync.fetchUserSync(any(String.class))).thenReturn(new FetchUserHttpEndpointSync.EndpointResult(FetchUserHttpEndpointSync.EndpointStatus.GENERAL_ERROR,
                 USER_ID, USER_NAME));
     }
 
     private void authError() throws NetworkErrorException {
-        when(fetchUserHttpEndpointSync.fetchUserSync(USER_ID)).thenReturn(new FetchUserHttpEndpointSync.EndpointResult(FetchUserHttpEndpointSync.EndpointStatus.AUTH_ERROR,
+        when(fetchUserHttpEndpointSync.fetchUserSync(any(String.class))).thenReturn(new FetchUserHttpEndpointSync.EndpointResult(FetchUserHttpEndpointSync.EndpointStatus.AUTH_ERROR,
                 USER_ID, USER_NAME));
     }
 
     private void networkError() throws NetworkErrorException {
         doThrow(new NetworkErrorException()).when(fetchUserHttpEndpointSync.fetchUserSync(any(String.class)));
+    }
+
+    private void cacheUser() {
+        when(usersCache.getUser(any(String.class))).thenReturn(new User(any(String.class), any(String.class)));
     }
 
 }
